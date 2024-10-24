c1863072e5a1a149c19637491f4c53bc8e99ef9e
Grigory Vasilev <grishavasilev62@gmail.com>
2024-10-24 18:09:52 +0300

    clean

---
+++ nl/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, Output } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+declare const localStorage: any;
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  
+}
